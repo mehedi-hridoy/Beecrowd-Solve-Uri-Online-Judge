1d51d4b5d511660f2907096e0b710ddcde98bfbb
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2022-10-01 14:05:03 +0600

    Codeforces_959A.cpp

---
+++ Codeforces_959A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+
+    if(x%2 == 0) {
+        cout<<"Mahmoud"<<endl;
+    }
+    if(x%2 !=0) {
+        cout<<"Ehab"<<endl;
+    }
+    return 0;
+}
