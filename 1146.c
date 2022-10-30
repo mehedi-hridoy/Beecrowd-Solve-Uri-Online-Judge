8db47b06a99ee17c55219101b00179becd426cec
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2022-10-30 14:03:41 +0600

    Growing Sequence

--- 1146.c
+++ 1146.c
@@ -14,3 +14,27 @@ int main(){
     }
 return 0;
 }
+
+
+//Another Solution made my me
+#include<iostream>
+using namespace std;
+int main()
+{
+    while(1){
+        int n;
+        cin>>n;
+        if(n==0){
+            break;
+        }
+        if(n>0){
+            for(int i=1;i<=n-1;i++){
+                cout<<i<<" ";
+               
+            }
+            cout<<n;
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
