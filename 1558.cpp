2c43ab0fec8a63ef8e4feea247c170e60c9e4952
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-01-04 18:35:05 +0600

    Making Kites

---
+++ 1558.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int x1,x2;
+        cin>>x1>>x2;
+        int ans = (x1*x2)/2;
+        cout<<ans<<" cm2"<<endl;
+    }
+    return 0;
+    
+
+}
