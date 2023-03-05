f50185ca83b9e1cfbfa8eb774a2a40aa6c2f4c7f
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-03-05 19:50:01 +0600

    Triangles and Regular Polygons

---
+++ 2058.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long int n;
+    cin>>n;
+
+    cout<<n-2<<endl;
+    
+
+
+}
