fd3df67b6b2504c174cea2314ffddba945b716d5
Mehedi Hasan Hridoy <108460231+mehedi-hridoy@users.noreply.github.com>
2023-03-18 16:34:39 +0600

    I am toorg

---
+++ 2581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        cin >> s1;
+        cout << "I am Toorg!" << endl;
+    }
+}
